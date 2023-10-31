bd11caccd4287ad7f71939f47dccc8037075b2ee
shonya3 <poeshonya3@gmail.com>
2023-10-31 18:58:42 +0300

    popup: create main func

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -9,28 +9,32 @@ declare global {
 
 VideoCategoriesElement.define();
 
-const element = document.querySelector('wc-video-categories')!;
-element.categories = await Storage.getOrDefault('categories', []);
+async function main() {
+	const element = document.querySelector('wc-video-categories')!;
+	element.categories = await Storage.getOrDefault('categories', []);
 
-element.addEventListener('delete-clicked', async e => {
-	const category = (e as CustomEvent<string>).detail;
-	const categories = element.categories.filter(cat => cat !== category);
-	await Storage.set('categories', categories);
-	element.categories = categories;
-});
-
-element.addEventListener('add-category', async e => {
-	const category = (e as CustomEvent<string>).detail;
-	const categories = element.categories.concat(category);
-	await Storage.set('categories', categories);
-	element.categories = categories;
-});
-
-element.addEventListener('reorder-categories', async e => {
-	const categories = (e as CustomEvent<string[]>).detail;
-	await Storage.set('categories', categories);
-
-	document.startViewTransition(() => {
+	element.addEventListener('delete-clicked', async e => {
+		const category = (e as CustomEvent<string>).detail;
+		const categories = element.categories.filter(cat => cat !== category);
+		await Storage.set('categories', categories);
 		element.categories = categories;
 	});
-});
+
+	element.addEventListener('add-category', async e => {
+		const category = (e as CustomEvent<string>).detail;
+		const categories = element.categories.concat(category);
+		await Storage.set('categories', categories);
+		element.categories = categories;
+	});
+
+	element.addEventListener('reorder-categories', async e => {
+		const categories = (e as CustomEvent<string[]>).detail;
+		await Storage.set('categories', categories);
+
+		document.startViewTransition(() => {
+			element.categories = categories;
+		});
+	});
+}
+
+main();
